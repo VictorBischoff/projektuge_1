628193ea80f6c857236044ae7898d24f11c36a90
victorbischoff <morkebergv@gmail.com>
2024-10-07 17:17:56 +0200

    updated question mark to show winning number

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // find elementer i html fil og lav variabler
 // Valider brugerens input
 // lav funktion til at finde et tilfældigt tal og matche det brugerens input
-const userNumberInput = document.querySelector('.guess');
 
+// get the input html element
+const userNumberInput = document.querySelector('.guess');
 // check button 
 const userNumberButton = document.querySelector('.check');
 
@@ -10,17 +11,18 @@ const userNumberButton = document.querySelector('.check');
 let userScore = document.querySelector('.score');
 // convert the score to a Number
 let currentUserScore = parseInt(userScore.innerHTML);
-
+// get highscore from html element
 let highScore = document.querySelector('.highscore');
+// convert the highscore to a Number
 let currentHighScore = parseInt(highScore.innerHTML);
 // again! button
 const againButton = document.querySelector('.again');
-
+// this element starts as a question mark, and should convert to winning number
 const topNumber = document.querySelector('.number');
 
 let randNumber = Math.floor(Math.random() * (21 - 1) + 1);
 console.log(randNumber);
-
+// validate that entered number is between 1 and 20
 function validateUserInput(userInput) {
   if (userInput > 0 && userInput < 21) {
     return true;
@@ -28,16 +30,20 @@ function validateUserInput(userInput) {
     return false;
   }
 }
-
+// helper function for clearing the input field
 function clearInputField(element) {
   element.value = '';
 }
-
+// helper function to determine if a new highscore is to be set 
 function updateHighScore(currentScore, currentHigh) {
   if (currentHigh < currentScore) {
     currentHigh = currentScore;
     highScore.innerHTML = currentHigh;
   }
+}
+
+function updateHtmlText(element, string) {
+  element.innerHTML = `${string}`;
 }
 
 function checkUserNumber(userInputCheck) {
@@ -48,6 +54,7 @@ function checkUserNumber(userInputCheck) {
       clearInputField(userNumberInput);
     } else {
       updateHighScore(currentUserScore, currentHighScore)
+      updateHtmlText(topNumber, userNumberInput.value);
       clearInputField(userNumberInput);
       alert('You guessed the number!')
     }
